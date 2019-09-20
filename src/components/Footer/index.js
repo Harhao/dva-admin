53aeba05feaefea75d6cd11c4d2a42addd7c3bc7
huahao <lvhuahao@ad61v1.com>
2019-09-20 16:25:35 +0800

    修复整个dashboard布局

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from './index.scss';
-
+import {Icon} from "antd"
 export default class Footer extends React.Component{
     render(){
         return(
             <div className={styles.footer}>
-
+                <Icon type="github" style={{fontSize:'24px',marginTop:'10px'}}/>
+                <div className={styles.info}>
+                    <span className={styles.project}>dva-admin</span>
+                </div>
+                <div className={styles.copyRight}>
+                    Copyright © {new Date().getFullYear()} Harhao
+                </div>
             </div>
         );
     }
